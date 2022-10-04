d8e84e5e4749a589b58abe2d9a87781c41cc8d4e
immprada <im.mprada@gmail.com>
2022-10-04 10:12:40 -0500

    add TopHangman::Render instance to TopHangman::Round class

--- lib/top_hangman/round.rb
+++ lib/top_hangman/round.rb
@@ -1,3 +1,5 @@
+require_relative 'render'
+
 module TopHangman
   class Round
     attr_reader :word
@@ -10,6 +12,12 @@ module TopHangman
       @guess_count = 0
       @word_array = word.split('')
       @word_progress = Array.new(@word_array.length, '_')
+      @render = Render.new
+    end
+
+    def try_guess
+      @guess = @render.ask_for_guess
+      @guesses << @guess
     end
   end
 end
