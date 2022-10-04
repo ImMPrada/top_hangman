92209be74daa0003bfc39e9925bc26e88c66356e
immprada <im.mprada@gmail.com>
2022-10-04 18:03:38 -0500

    add TopHangman::Render spec

---
+++ spec/lib/top_hangman/render_spec.rb
@@ -0,0 +1,21 @@
+require './spec/spec_helper'
+
+RSpec.describe TopHangman::Render do
+  let(:render) { described_class.new }
+
+  describe '#ask_for_guess' do
+    let(:input_letter) { 'a' }
+
+    before do 
+      allow(render).to receive(:gets).and_return("#{input_letter}\n")
+    end
+
+    it 'returns a string' do
+      expect(render.ask_for_guess).to be_a(String)
+    end
+
+    it 'returns inputed string' do
+      expect(render.ask_for_guess).to eq(input_letter)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'byebug'
 require './lib/top_hangman/game'
 require './lib/top_hangman/round'
+require './lib/top_hangman/render'
 
 RSpec.configure do |config|
   # Use the specified formatter
