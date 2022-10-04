a256a27c062d34cced397c78806a828096ed84c5
immprada <im.mprada@gmail.com>
2022-10-03 20:33:59 -0500

    add TopHangman::Board, initialize

--- lib/top_hangman/board.rb
+++ lib/top_hangman/board.rb
@@ -1,4 +1,25 @@
 module TopHangman
   class Board
+    NUMBER_OF_STATES = 7
+    FOLDER_NAME = 'lib/hangmans'.freeze
+
+    attr_reader :states
+
+    def initialize
+      @states = read_hangman_files
+    end
+
+    private
+
+    def read_hangman_files
+      hangman_states = {}
+      NUMBER_OF_STATES.times { |i| hangman_states[i] = open_file(i).readlines.map(&:chomp) }
+
+      hangman_states
+    end
+
+    def open_file(index)
+      File.open("#{FOLDER_NAME}/state_0#{index}.txt", 'r')
+    end
   end
 end
