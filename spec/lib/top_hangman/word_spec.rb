c43212b65c641184bce3fb42cc2605ff5bcfc444
immprada <im.mprada@gmail.com>
2022-10-05 11:15:41 -0500

    add TopHangman::Word #set_current_word spec

--- spec/lib/top_hangman/word_spec.rb
+++ spec/lib/top_hangman/word_spec.rb
@@ -19,18 +19,18 @@ RSpec.describe TopHangman::Word do
     end
   end
 
-  # describe '#start' do
-  #   let(:game) { described_class.new('google-10000-english-no-swears.txt') }
-  #   let(:input_letter) { 'a' }
+  describe '#set_current_word' do
+    before do
+      word.set_current_word
+    end
 
-  #   before do
-  #     allow(described_class).to receive(:gets).and_return(input_letter)
-  #     game.start
-  #   end
+    it 'sets @current_word to a random word' do
+      expect(word.current_word).to be_a(String)
+    end
 
-  #   it 'sets @round to Round class object' do
-  #     round = game.instance_variable_get(:@round).class
-  #     expect(round).to be(TopHangman::Round)
-  #   end
-  # end
+    it 'sets @history_of_words to an array of words' do
+      history_of_words = word.instance_variable_get(:@history_of_words)
+      expect(history_of_words).to be_an(Array)
+    end
+  end
 end
