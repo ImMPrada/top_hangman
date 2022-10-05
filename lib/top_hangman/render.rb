e9689febdeef9557fb3419a7cd5da3e0f6369a09
immprada <im.mprada@gmail.com>
2022-10-05 13:21:32 -0500

    delete TopHangman::Board, move states of hangman to TopHangman::Rener

--- lib/top_hangman/render.rb
+++ lib/top_hangman/render.rb
@@ -1,8 +1,32 @@
 module TopHangman
   class Render
+    NUMBER_OF_STATES = 7
+    FOLDER_NAME = 'lib/hangmans'.freeze
+
+    def initialize
+      @states = read_hangman_files
+    end
+
     def ask_for_guess
       puts 'Guess a letter:'
       gets.chomp
+    end
+
+    private
+
+    def read_hangman_files
+      hangman_states = {}
+      NUMBER_OF_STATES.times { |i| hangman_states[i] = open_file(i) }
+
+      hangman_states
+    end
+
+    def open_file(index)
+      file = File.open("#{FOLDER_NAME}/state_0#{index}.txt", 'r')
+      lines_read = file.readlines.map(&:chomp)
+      file.close
+
+      lines_read
     end
   end
 end
--- spec/lib/top_hangman/render_spec.rb
+++ spec/lib/top_hangman/render_spec.rb
@@ -3,6 +3,27 @@ require './spec/spec_helper'
 RSpec.describe TopHangman::Render do
   let(:render) { described_class.new }
 
+  describe '#initialize' do
+    it 'sets @states to a hash of 7 elements' do
+      states = render.instance_variable_get(:@states)
+      expect(states.size).to be(7)
+    end
+
+    it 'sets each value of @states to an array of 14 elements' do
+      states = render.instance_variable_get(:@states)
+      states.each_value do |value|
+        expect(value.size).to be(14)
+      end
+    end
+
+    it 'sets each value of @states to an array of strings' do
+      states = render.instance_variable_get(:@states)
+      states.each_value do |value|
+        value.all? { |line| expect(line).to be_a(String) }
+      end
+    end
+  end
+
   describe '#ask_for_guess' do
     let(:input_letter) { 'a' }
 
