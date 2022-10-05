4db456f247b6a1ecd91d5c4f298932cdad5fdcc8
immprada <im.mprada@gmail.com>
2022-10-05 11:24:44 -0500

    update TopHangman::Game class by implementation of TopHangman::Word

--- lib/top_hangman/game.rb
+++ lib/top_hangman/game.rb
@@ -1,16 +1,12 @@
 require_relative 'round'
+require_relative 'word'
 
 module TopHangman
   class Game
-    MIN_WORD_SIZE = 5
-    MAX_WORD_SIZE = 12
+    # attr_reader :base_words
 
-    attr_reader :base_words
-
-    def initialize(file_name)
-      @base_words = read_words_file(file_name)
-      @list_of_words_size = base_words&.size
-      @history_of_words = []
+    def initialize
+      @word = Word.new
     end
 
     def start
@@ -25,37 +21,7 @@ module TopHangman
 
     private
 
-    def read_words_file(file_name)
-      return nil unless File.exist?(file_name)
-
-      base_file = File.open(file_name, 'r')
-      base_file.readlines.map(&:chomp).select { |line| line.size > MIN_WORD_SIZE && line.size < MAX_WORD_SIZE }
-    end
-
-    def random_word
-      return nil unless base_words
-
-      random_index = rand(0..@list_of_words_size)
-      random_word = base_words[random_index]
-      base_words.delete_at(random_index)
-      @list_of_words_size -= 1
-
-      random_word
-    end
-
-    def word_in_history?(word)
-      @history_of_words.include?(word)
-    end
-
-    def add_word_to_history(word)
-      @history_of_words << word
-    end
-
     def create_round
-      return nil unless base_words || @list_of_words_size == @history_of_words.size
-
-      word = random_word
-      add_word_to_history(word)
       Round.new(word)
     end
   end
