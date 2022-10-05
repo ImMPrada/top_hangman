3bdfc80ddd668a6ee0eb5be344aed54caa995b74
immprada <im.mprada@gmail.com>
2022-10-04 19:17:54 -0500

    update TopHangman::Game #start spec

--- spec/lib/top_hangman/game_spec.rb
+++ spec/lib/top_hangman/game_spec.rb
@@ -52,18 +52,18 @@ RSpec.describe TopHangman::Game do
     end
   end
 
-  # describe '#start' do
-  #   let(:game) { described_class.new('google-10000-english-no-swears.txt') }
-  #   let(:render) { TopHangman::Render.new }
-  #   let(:input_letter) { 'a' }
+  describe '#start' do
+    let(:game) { described_class.new('google-10000-english-no-swears.txt') }
+    let(:input_letter) { 'a' }
 
-  #   before do
-  #     game.start
-  #   end
+    before do
+      allow(game).to receive(:gets).and_return(input_letter)
+      game.start
+    end
 
-  #   it 'sets @round to Round class object' do
-  #     round = game.instance_variable_get(:@round).class
-  #     expect(round).to be(TopHangman::Round)
-  #   end
-  # end
+    it 'sets @round to Round class object' do
+      round = game.instance_variable_get(:@round).class
+      expect(round).to be(TopHangman::Round)
+    end
+  end
 end
