c012468798b60bd17936329d3b28f745cd320e23
immprada <im.mprada@gmail.com>
2022-10-05 17:05:43 -0500

    update TopHangman::Round

--- lib/top_hangman/round.rb
+++ lib/top_hangman/round.rb
@@ -1,47 +1,39 @@
 require_relative 'render'
+require_relative 'guess'
 
 module TopHangman
   class Round
     attr_reader :word
 
-    def initialize(word)
+    def initialize(word, render)
       @word = word
-      @end_of_round = false
-      @guesses = []
-      @guess = nil
-      @guess_count = 0
-      @word_array = word.split('')
-      @word_progress = Array.new(@word_array.length, '_')
-      @render = Render.new
+      @guess = Guess.new(@word.current_word, render)
+      @render = render
     end
 
     def start_round
-      @guess = @render.ask_for_guess
-
       run_round_loop
+
+      return :you_lose if mistakes_limit_reached?
+
+      :you_win
     end
 
     def run_round_loop
-      check_guess
-      update_guesses_history
+      puts @word.current_word
+      puts @guess.mistakes_count
+
+      @guess.run_loop
+      puts mistakes_limit_reached?
+      run_round_loop unless word_revealed? || mistakes_limit_reached?
     end
 
-    def update_guesses_history
-      @guesses << @guess
+    def mistakes_limit_reached?
+      @guess.mistakes_count == 7
     end
 
-    private
-
-    def check_guess
-      @is_guess_repeated_or_in_word = false
-      return @is_guess_repeated_or_in_word = true if @guesses.include?(@guess)
-
-      @word_array.each_with_index do |letter, index|
-        next unless letter == @guess
-
-        @word_progress[index] = letter
-        @is_guess_repeated_or_in_word = true
-      end
+    def word_revealed?
+      @guess.word_progress.join('') == @word.current_word
     end
   end
 end
