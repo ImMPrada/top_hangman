e901f17e9be9f000256bb1048dbd7878f01b3ea4
immprada <im.mprada@gmail.com>
2022-10-06 18:02:10 -0500

    refactor TopHangman::Guess

--- lib/top_hangman/guess.rb
+++ lib/top_hangman/guess.rb
@@ -1,62 +1,22 @@
-require_relative 'render'
-
 module TopHangman
   class Guess
+    REPEATED = :repeated_guess
+    WRONG = :wrong_guess
+    RIGHT = :right_guess
+
     attr_reader :word, :mistakes_count, :word_progress, :guess_tries
 
-    def initialize(word, render)
-      @guess_tries = []
-      @current_guess = nil
-      @mistakes_count = 0
-      @word_array = word.split('')
-      @word_progress = Array.new(@word_array.length, '_')
+    def initialize(attempted_letter, word)
+      @attempted_letter = attempted_letter
       @word = word
-      @render = render
     end
 
-    def try_guess
-      @current_guess = @render.ask_for_guess
-      check_guess
+    def validate
+      @word.letter_present?(@attempted_letter) ? RIGHT : WRONG
     end
 
-    def run_loop
-      check_response_code = try_guess
-
-      show_guess_try_message(check_response_code)
-      check_response_code
-    end
-
-    private
-
-    attr_reader :current_guess
-
-    def guess_repeated?
-      @guess_tries.include?(current_guess)
-    end
-
-    def check_guess
-      return :repeated_guess if guess_repeated?
-
-      response_code = :guess_wrong
-      @guess_tries << @current_guess
-
-      @word_array.each_with_index do |letter, index|
-        next unless letter == current_guess
-
-        @word_progress[index] = letter
-        response_code = :guess_correct
-      end
-
-      response_code
-    end
-
-    def show_guess_try_message(check_response_code)
-      case check_response_code
-      when :guess_wrong
-        @mistakes_count += 1
-      when :guess_correct
-        @render.show_correct_guess_message(@word_progress)
-      end
+    def repeated_letter?(history)
+      history.any? { |old_guess| old_guess.attempted_letter == @attempted_letter }
     end
   end
 end
