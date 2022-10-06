6d3c17a4365784372b3e353cb99468a6c6bad933
immprada <im.mprada@gmail.com>
2022-10-06 12:40:54 -0500

    upgrade TopHangman::Guess

--- lib/top_hangman/guess.rb
+++ lib/top_hangman/guess.rb
@@ -2,7 +2,7 @@ require_relative 'render'
 
 module TopHangman
   class Guess
-    attr_reader :word, :mistakes_count, :word_progress
+    attr_reader :word, :mistakes_count, :word_progress, :guess_tries
 
     def initialize(word, render)
       @guess_tries = []
@@ -10,6 +10,7 @@ module TopHangman
       @mistakes_count = 0
       @word_array = word.split('')
       @word_progress = Array.new(@word_array.length, '_')
+      @word = word
       @render = render
     end
 
@@ -51,10 +52,7 @@ module TopHangman
 
     def show_guess_try_message(check_response_code)
       case check_response_code
-      when :repeated_guess
-        @render.show_repeated_guess_message
       when :guess_wrong
-        @render.show_wrong_guess_message
         @mistakes_count += 1
       when :guess_correct
         @render.show_correct_guess_message(@word_progress)
