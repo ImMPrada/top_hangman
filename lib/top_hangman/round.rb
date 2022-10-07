3e476be3b97d7651b08addca8b233c280e5e74c4
immprada <im.mprada@gmail.com>
2022-10-06 19:31:34 -0500

    refactor TopHangman::Round

--- lib/top_hangman/round.rb
+++ lib/top_hangman/round.rb
@@ -1,29 +1,67 @@
-require_relative 'render'
+require_relative 'word'
 require_relative 'guess'
 
 module TopHangman
   class Round
-    attr_reader :word
+    ERRORS_LIMIT = 7
+    RUNNING = :running
+    STOPPED = :stopped
+
+    WON = :won
+    LOST = :lost
+
+    attr_reader :word, :current_guess, :errors_count, :state
 
     def initialize
       @word = Word.from_list
       @guess_history = []
-      @word_history = []
       @current_guess = nil
+      @errors_count = 0
+      @state = RUNNING
     end
 
     def create_guess(letter)
       @current_guess = Guess.new(letter, @word)
     end
 
-    def determine_guess_state
-      return Guess::REPEATED if @guess.repeated_letter?(@guess_history)
+    def update_state
+      validation_result = @current_guess.validate(@guess_history)
 
-      @guess.validate
+      case validation_result
+      when Guess::WRONG
+        update_errors_count
+        update_guess_history
+      when Guess::RIGHT
+        update_word_progress
+        update_guess_history
+      end
+
+      @state = STOPPED and return if finished?
     end
 
+    def result
+      return unless finished?
+
+      return WON if @word.progress.all?(true)
+      return LOST if @errors_count == ERRORS_LIMIT
+    end
+
+    def finished?
+      @word.progress.all?(true) || @errors_count == ERRORS_LIMIT
+    end
+
+    private
+
     def update_guess_history
-      @guess_history << @guess
+      @guess_history << @current_guess
+    end
+
+    def update_errors_count
+      @errors_count += 1
+    end
+
+    def update_word_progress
+      @word.update_progress_with(@current_guess.attempted_letter)
     end
   end
 end
