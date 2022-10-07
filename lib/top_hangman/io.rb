1b6bda04b1acd3619f52cdf9ddf52ae02c450718
immprada <im.mprada@gmail.com>
2022-10-06 20:51:34 -0500

    add game loop to TopHangman::IO

--- lib/top_hangman/io.rb
+++ lib/top_hangman/io.rb
@@ -3,30 +3,43 @@ module TopHangman
     class << self
       def execute(game, renderer)
         game.start
-        renderer.show_header(errors_count: game.current_round.errors_count)
 
-        while game.current_round.state == TopHangman::Round::RUNNING
-          renderer.ask_for_guess
-          guess_letter = gets.chomp
-          game.play_round(guess_letter)
-          renderer.show_progress(
-            errors_count: game.current_round.errors_count,
-            guess_history: game.current_round.guess_history,
-            word: game.current_round.word
-          )
-        end
+        while game.state == Game::RUNNING
+          renderer.show_header(errors_count: game.current_round.errors_count)
 
-        case game.current_round.result
-        when TopHangman::Round::WON
-          renderer.winning_message(
-            errors_count: game.current_round.errors_count,
-            word: game.current_round.word
-          )
-        when TopHangman::Round::LOST
-          renderer.losing_message(
-            errors_count: game.current_round.errors_count,
-            word: game.current_round.word
-          )
+          while game.current_round.state == TopHangman::Round::RUNNING
+            renderer.ask_for_guess
+            guess_letter = gets.chomp
+            game.play_round(guess_letter)
+            renderer.show_progress(
+              errors_count: game.current_round.errors_count,
+              guess_history: game.current_round.guess_history,
+              word: game.current_round.word
+            )
+          end
+
+          case game.current_round.result
+          when TopHangman::Round::WON
+            renderer.winning_message(
+              errors_count: game.current_round.errors_count,
+              word: game.current_round.word
+            )
+          when TopHangman::Round::LOST
+            renderer.losing_message(
+              errors_count: game.current_round.errors_count,
+              word: game.current_round.word
+            )
+          end
+          gets
+
+          renderer.ask_for_new_round
+          answer = gets.chomp.upcase
+
+          if answer == 'Y'
+            game.create_new_round
+          else
+            game.stop
+          end
         end
       end
     end
