778c3ebacb476a51efbc1c8826ca03db91f07075
immprada <im.mprada@gmail.com>
2022-10-09 13:31:31 -0500

    add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,20 @@
+require 'simplecov'
+require 'simplecov-lcov'
+
 require 'byebug'
 require './lib/top_hangman/game'
 require './lib/top_hangman/round'
-require './lib/top_hangman/render'
 require './lib/top_hangman/word'
 require './lib/top_hangman/guess'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.output_directory = '.'
+  c.lcov_file_name = 'lcov.info'
+  c.single_report_path = './lcov.info'
+end
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start
 
 RSpec.configure do |config|
   # Use the specified formatter
