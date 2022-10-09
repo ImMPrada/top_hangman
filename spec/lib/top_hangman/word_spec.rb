5dfbe928d18b8b189cf1ed5defff87af1f300449
immprada <im.mprada@gmail.com>
2022-10-09 18:10:37 -0500

    update TopHangman::Word #initialize spec

--- spec/lib/top_hangman/word_spec.rb
+++ spec/lib/top_hangman/word_spec.rb
@@ -1,21 +1,17 @@
 require './spec/spec_helper'
 
 RSpec.describe TopHangman::Word do
-  let(:word) { described_class.new }
+  let(:test_string) { 'test_string' }
 
   describe '#initialize' do
-    it 'sets @current_word to nil' do
-      expect(word.current_word).to be_nil
+    let(:word) { described_class.new(test_string) }
+
+    it 'sets the value' do
+      expect(word.value).to eq(test_string)
     end
 
-    it 'sets @history_of_words to nil' do
-      history_of_words = word.instance_variable_get(:@history_of_words)
-      expect(history_of_words).to be_empty
-    end
-
-    it 'sets @base_words to an array of words' do
-      base_words = word.instance_variable_get(:@base_words)
-      expect(base_words).to be_an(Array)
+    it 'sets the progress' do
+      expect(word.progress).to eq(Array.new(test_string.size, false))
     end
   end
 
