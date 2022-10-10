d0cbb47d18e77df281bb34f4ed16533cfa5efe51
immprada <im.mprada@gmail.com>
2022-10-10 05:54:37 -0500

    add TopHangman::Round #crete_guess spec

--- spec/lib/top_hangman/round_spec.rb
+++ spec/lib/top_hangman/round_spec.rb
@@ -1,22 +1,27 @@
 require './spec/spec_helper'
 
 RSpec.describe TopHangman::Round do
-  let(:word) { TopHangman::Word.new }
-  let(:render) { TopHangman::Render.new }
-  let(:round) { described_class.new(word, render) }
+  let(:round) { described_class.new }
 
-  describe '#initialize' do
-    before do
-      word.set_current_word
+  before do
+    word_instance = instance_double(
+      TopHangman::Word,
+      value: 'ruby',
+      progress: Array.new('ruby'.size, false)
+    )
+    allow(TopHangman::Word).to receive(:from_list).and_return(word_instance)
+  end
+
+  describe '#crete_guess' do
+    it 'creates a new guess' do
+      round.create_guess('a')
+      expect(round.current_guess).to be_a(TopHangman::Guess)
     end
 
-    it 'returns Round class object' do
-      expect(round).to be_a(described_class)
-    end
-
-    it 'sets @word to word' do
-      word = round.instance_variable_get(:@word)
-      expect(word).to eq(word)
+    it 'sets the guess attempted letter' do
+      round.create_guess('a')
+      current_guess = round.instance_variable_get(:@current_guess)
+      expect(current_guess.attempted_letter).to eq('a')
     end
   end
 end
