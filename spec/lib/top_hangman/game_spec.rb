1ad4ed12193a0b73f07f0526fd38517a0b92a86c
immprada <im.mprada@gmail.com>
2022-10-10 10:33:44 -0500

    add TopHangman::Game #create_new_round spec

--- spec/lib/top_hangman/game_spec.rb
+++ spec/lib/top_hangman/game_spec.rb
@@ -1,7 +1,7 @@
 require './spec/spec_helper'
 
 RSpec.describe TopHangman::Game do
-  let(:game) { TopHangman::Game.new }
+  let(:game) { described_class.new }
 
   describe '#start' do
     before { game.start }
@@ -29,6 +29,32 @@ RSpec.describe TopHangman::Game do
 
     it 'updates the state of the current round to running' do
       expect(game.current_round.state).to be(TopHangman::Round::RUNNING)
+    end
+  end
+
+  describe '#create_new_round' do
+    let(:letter) { 'a' }
+
+    before do
+      game.start
+      game.play_round(letter)
+    end
+
+    it 'creates a Round object in current_round' do
+      game.create_new_round
+      expect(game.current_round).to be_a(TopHangman::Round)
+    end
+
+    it 'adds the current round to the history' do
+      old_round = game.current_round
+      game.create_new_round
+      expect(game.rounds_history).to include(old_round)
+    end
+
+    it 'creates a new round' do
+      old_round = game.current_round
+      game.create_new_round
+      expect(game.current_round).not_to be(old_round)
     end
   end
 end
