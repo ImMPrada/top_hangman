ea1b6b3e955dc9f0ed533938874b9631c05cf43f
immprada <im.mprada@gmail.com>
2022-10-10 10:23:49 -0500

    add TopHangman::Game #start spec

--- spec/lib/top_hangman/game_spec.rb
+++ spec/lib/top_hangman/game_spec.rb
@@ -1,5 +1,17 @@
 require './spec/spec_helper'
 
 RSpec.describe TopHangman::Game do
+  let(:game) { TopHangman::Game.new }
 
+  describe '#start' do
+    before { game.start }
+
+    it 'sets the state to running' do
+      expect(game.state).to eq(TopHangman::Game::RUNNING)
+    end
+
+    it 'creates a new round' do
+      expect(game.current_round).to be_a(TopHangman::Round)
+    end
+  end
 end
